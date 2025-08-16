cfd477595600687b86da865fd07d7218a3b37c58
Mohamed Abdelfattah <mhabdelfattah22@gmail.com>
2025-08-17 00:11:54 +0300

    Integrate Supabase service for file storage operations in test case handlers and commands. Add `CreateFileWithNameAsync` to TempCodeFileService.

--- src/CodeForge.Application/Testcases/Commands/UpdateTestcase/UpdateTestcaseCommandHandler.cs
+++ src/CodeForge.Application/Testcases/Commands/UpdateTestcase/UpdateTestcaseCommandHandler.cs
@@ -1,15 +1,21 @@
 ﻿using Codeforge.Domain.Entities;
 using Codeforge.Domain.Exceptions;
+using Codeforge.Domain.Interfaces;
+using Codeforge.Domain.Options;
 using Codeforge.Domain.Repositories;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Codeforge.Application.Testcases.Commands.UpdateTestcase;
 
 public class UpdateTestcaseCommandHandler(
 	ILogger<UpdateTestcaseCommandHandler> logger,
-	ITestcasesRepository testcasesRepository) : IRequestHandler<UpdateTestcaseCommand> {
+	ITestcasesRepository testcasesRepository,
+	ISupabaseService supabaseService,
+	ITempCodeFileService fileService,
+	IOptions<SupabaseOptions> supabaseOptions) : IRequestHandler<UpdateTestcaseCommand> {
 	public async Task Handle(UpdateTestcaseCommand request, CancellationToken cancellationToken) {
 		logger.LogInformation("Updating testcase with ID {TestcaseId}", request.TestcaseId);
 
@@ -18,10 +24,21 @@ public class UpdateTestcaseCommandHandler(
 		var testcase = await testcasesRepository.GetByIdAsync(request.TestcaseId);
 		if (testcase is null) throw new NotFoundException(nameof(TestCase), request.TestcaseId.ToString());
 
-		testcase.Input = request.Input ?? testcase.Input;
-		testcase.ExpectedOutput = request.ExpectedOutput ?? testcase.ExpectedOutput;
+
+		if (request.Input is not null)
+			await UpdateSupabaseFileAsync(supabaseOptions.Value.Bucket, testcase.Input, request.Input);
+
+		if (request.ExpectedOutput is not null)
+			await UpdateSupabaseFileAsync(supabaseOptions.Value.Bucket, testcase.ExpectedOutput, request.ExpectedOutput);
+
 		testcase.IsVisible = request.IsVisible ?? testcase.IsVisible;
 
 		await testcasesRepository.UpdateAsync(testcase);
+	}
+
+	private async Task UpdateSupabaseFileAsync(string bucket, string fileName, string content) {
+		var tempFile = await fileService.CreateFileWithNameAsync(fileName, content);
+		await supabaseService.UploadOrUpdateFileAsync(bucket, tempFile);
+		fileService.DeleteTempFile(tempFile);
 	}
 }
